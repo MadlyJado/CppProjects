45e414e245a409b0b4b0f3e5bd88859af284c2bc
MadlyJado <jadonchenard@gmail.com>
2021-09-07 00:19:03 -0400

    Created Cpp file to work with course lesson exercise suggestion

---
+++ SizeOfFloats/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cfloat>
+
+using namespace std;
+
+int main() {
+
+    cout << "float: " << sizeof(float) << " bytes." << endl;
+    cout << "double: " << sizeof(double) << " bytes." << endl;
+    cout << "long double: " << sizeof(long double) << " bytes." << endl;
+
+    return 0;
+}
+
